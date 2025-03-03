3f1947161648647083118ef75ceb42fb084bb644
Mr.Mao <951416545@qq.com>
2025-03-03 22:04:25 +0800

    chore: update view

--- src/components/ui/appearance-switch.tsx
+++ src/components/ui/appearance-switch.tsx
@@ -1,11 +1,15 @@
 import { useDark } from 'jotai-dark/react'
+import { useEffect } from 'react'
 
 export function AppearanceSwitch({ className = '' }: { className?: string }) {
-  const { toggleDark } = useDark({
+  const { toggleDark, isDark } = useDark({
     disableTransition: true,
     disableTransitionExclude: ['.i-lucide-sun', '.i-lucide-moon'],
   })
-
+  useEffect(() => {
+    if (!isDark) 
+      toggleDark()
+  }, [])
   return (
     <button type="button" disabled onClick={toggleDark} className={`flex ${className} text-gray-5`}>
       <div className="i-lucide-sun scale-100 dark:scale-0 transition-transform duration-500 rotate-0 dark:-rotate-90" />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html suppressHydrationWarning lang="en">
+    <Html className='dark' suppressHydrationWarning lang="en">
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
